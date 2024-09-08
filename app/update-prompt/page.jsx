0473889e45e97e2cc681ac59a3a4d3f5f4eb80f1
Navindu <dahanayakenw.20@uom.lk>
2024-09-09 01:34:00 +0530

    Edit prompt completed

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Form from "@components/Form";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const UpdatePrompt = () => {
+   const { data: session } = useSession();
+   const router = useRouter();
+   const searchParams = useSearchParams();
+   const promptId = searchParams.get("id");
+
+   const [submitting, setSubmitting] = useState(false);
+
+   const [post, setPost] = useState({
+      prompt: "",
+      tag: "",
+   });
+
+   const fetchPost = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      setPost({
+            prompt: data.prompt,
+            tag: data.tag,
+      });
+   };
+
+   useEffect(() => {
+      if(promptId) fetchPost();
+   }, [promptId]);
+
+   const editPrompt = async (e) => {
+      e.preventDefault();
+      setSubmitting(true);
+
+      if(!promptId) return alert("Prompt not found");
+
+      try {
+         const response = await fetch(`/api/prompt/${promptId}`, {
+            method: "PATCH",
+            body: JSON.stringify({
+               prompt: post.prompt,
+               tag: post.tag,
+            }),
+         });
+
+         if (response.ok) {
+            router.push("/profile");
+         }
+      } catch (error) {
+         console.log(error);
+      } finally {
+         setSubmitting(false);
+      }
+   };
+
+   return (
+      <Form
+         type="Update"
+         post={post}
+         setPost={setPost}
+         submitting={submitting}
+         handleSubmit={editPrompt}
+      />
+   );
+};
+
+export default UpdatePrompt;
