f7cbe635235967cc8bb4ea4e8b31e1c3805097e5
Navindu <dahanayakenw.20@uom.lk>
2024-09-09 20:04:38 +0530

    Search by prompt, email and tag implmented, with click on tag

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -21,8 +21,33 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
    const [searchText, setSearchText] = useState("");
    const [allPosts, setAllPosts] = useState([]);
+   const [searchResult, setSearchResult] = useState([]);
+   const [searchTimeout, setSearchTimeout] = useState(null);
 
-   const handleSearchChange = (e) => {};
+   const handleSearchChange = (e) => {
+      e.preventDefault();
+      clearTimeout(searchTimeout);
+      setSearchText(e.target.value);
+
+      setSearchTimeout(
+         setTimeout(() => {
+            const searchResult = querySearch(e.target.value);
+            setSearchResult(searchResult);
+         }, 500)
+      );
+   };
+
+   const querySearch = (text) => {
+      const regex = new RegExp(text, "i");
+
+      return allPosts.filter((post) => {
+         return (
+            regex.test(post.prompt) ||
+            regex.test(post.creator.email) ||
+            regex.test(post.tag)
+         );
+      });
+   };
 
    useEffect(() => {
       const fetchPost = async () => {
@@ -33,6 +58,11 @@ const Feed = () => {
 
       fetchPost();
    }, []);
+
+   const handleTagClick = (tag) => {
+      setSearchText(tag);
+      setSearchResult(querySearch(tag));
+   }
 
    return (
       <section className="feed">
@@ -47,7 +77,11 @@ const Feed = () => {
             ></input>
          </form>
 
-         <PromptCardList data={allPosts} handleTagClick={() => {}} />
+         {searchText ? (
+            <PromptCardList data={searchResult} handleTagClick={handleTagClick} />
+         ) : (
+            <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+         )}
       </section>
    );
 };
