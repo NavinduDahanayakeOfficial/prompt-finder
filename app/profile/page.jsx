16c36d51edb64848736a413f3cc490ace9405d35
Navindu <dahanayakenw.20@uom.lk>
2024-09-09 20:05:12 +0530

    rectify spelling mistakes

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,8 +4,7 @@ import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/profile";
-import { Router } from "next/router";
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
    const { data: session, status } = useSession();
