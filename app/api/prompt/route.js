bcf9b973a0865108c4de28caa354a7470312bad4
Navindu <dahanayakenw.20@uom.lk>
2024-09-19 21:03:19 +0530

    added cache control headers

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,7 +6,12 @@ export const GET = async (request) => {
    try {
       await connectToDB();
       const prompts = await Prompt.find().populate("creator");
-      return new Response(JSON.stringify(prompts), { status: 200 });
+      return new Response(JSON.stringify(prompts), {
+         status: 200,
+         headers: {
+            "Cache-Control": "s-maxage=60, stale-while-revalidate=30", // Cache for 60 seconds, allow stale for 30 seconds
+         },
+      });
    } catch (error) {
       return new Response("Failed to fetch prompts", { status: 500 });
    }
