89a04d6ad9073eef93438648ae81a18590f3b824
Navindu <dahanayakenw.20@uom.lk>
2024-09-19 21:07:48 +0530

    Revert "added cache control headers"
    This reverts commit bcf9b973a0865108c4de28caa354a7470312bad4.

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,12 +6,7 @@ export const GET = async (request) => {
    try {
       await connectToDB();
       const prompts = await Prompt.find().populate("creator");
-      return new Response(JSON.stringify(prompts), {
-         status: 200,
-         headers: {
-            "Cache-Control": "s-maxage=60, stale-while-revalidate=30", // Cache for 60 seconds, allow stale for 30 seconds
-         },
-      });
+      return new Response(JSON.stringify(prompts), { status: 200 });
    } catch (error) {
       return new Response("Failed to fetch prompts", { status: 500 });
    }
